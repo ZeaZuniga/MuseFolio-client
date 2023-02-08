23ce7ef90e0fe9e226fb1a19432d082d29c149f9
Zealtiel <zealtiel@outlook.com>
2023-02-07 19:45:43 -0600

    Add axios call for homePage.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,32 +2,39 @@ import LogInOut from "../../components/LogInOut/LogInOut";
 import "./HomePage.scss";
 import { useEffect, useState } from "react";
 import SongCard from "../../components/SongCard/SongCard";
+import axios from "axios";
 
 export default function Homepage() {
-  // const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
-  // useEffect(()=>{
-  //   const getFavorites = async()=>{
-  //     const {data} = await axios.get("http://localhost:8080/favorites");
-  //     setFavorites(data);
-  //   }
+  useEffect(() => {
+    const getFavorites = async () => {
+      const { data } = await axios.get("http://localhost:8080/favorites");
+      setFavorites(data);
+    };
 
-  //   getFavorites()
-  // }, [])
+    getFavorites();
+  }, []);
 
-  return (
-    <div className="homepage">
-      <LogInOut />
-      <h1 className="homepage__header">Let's start practicing!</h1>
-      <section className="favorite-songs">
-        <SongCard />
-        <SongCard />
-        <SongCard />
-        <SongCard />
-        <SongCard />
-        <SongCard />
-        <SongCard />
-      </section>
-    </div>
-  );
+  if (!favorites[0]) {
+    return (
+      <div className="homepage">
+        <LogInOut />
+        <h1 className="homepage__header">Let's start practicing!</h1>
+        <h3>Try uploading or favoriting some music to get started.</h3>
+      </div>
+    );
+  } else {
+    const favoritesList = favorites.map((song) => {
+      return <SongCard key={song.id} id={song.id} title={song.title} />;
+    });
+
+    return (
+      <div className="homepage">
+        <LogInOut />
+        <h1 className="homepage__header">Let's start practicing!</h1>
+        <section className="favorite-songs">{favoritesList}</section>
+      </div>
+    );
+  }
 }
