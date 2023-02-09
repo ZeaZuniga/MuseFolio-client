3b39b48e829b9a61275efd8100c108582811273b
Zealtiel <zealtiel@outlook.com>
2023-02-08 22:26:30 -0600

    Get pdf displayed, no styling.

--- src/pages/MusicPage/MusicPage.js
+++ src/pages/MusicPage/MusicPage.js
@@ -1,27 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 
 export default function MusicPage() {
   const [songDetails, setSongDetails] = useState([]);
   const currentSong = useParams().songId;
 
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
   useEffect(() => {
     const getSongDetails = async () => {
-      const { data } = await axios.get(`http://localhost:8080/${currentSong}`);
+      const { data } = await axios.get(
+        `http://localhost:8080/song/${currentSong}`
+      );
       setSongDetails(data[0]);
     };
 
     getSongDetails();
   }, []);
 
+  const onPdfSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   if (!songDetails.id) {
     return;
   } else {
+    console.log(songDetails.url_path);
+
     return (
       <div>
-        This is the MusicPage for {songDetails.title} by {songDetails.composer}
-        !!
+        <p>
+          This is the MusicPage for {songDetails.title} by
+          {songDetails.composer}!!
+        </p>
+        <Document file={songDetails.url_path} onLoadSuccess={onPdfSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
       </div>
     );
   }
